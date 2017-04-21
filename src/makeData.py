04c6a062211cec2cb5d7de97554ec593c73c4e61
LIJING <lijinging@outlook.com>
2017-04-21 14:07:28 +0800

    生成.mat数据集

--- src/makeData.py
+++ src/makeData.py
@@ -6,14 +6,16 @@ import numpy
 from PIL import Image
 
 dir_path = r'../pic_gauss/'
+raw_path = r'../pic_raw/'
 mat_path = r'../data/'
-mat_name = 'data.mat'
 testNum = 100
 
 
 # 以下创建mat文件
-label = numpy.zeros((0, 65536), dtype="int")
-data = numpy.zeros((0, 65536), dtype="int")
+train_label = numpy.zeros((0, 65536), dtype="int")
+train_data = numpy.zeros((0, 65536), dtype="int")
+test_label = numpy.zeros((0, 65536), dtype="int")
+test_data = numpy.zeros((0, 65536), dtype="int")
 
 filelist = os.listdir(dir_path)
 print(len(filelist))
@@ -21,13 +23,19 @@ cnt = 0
 
 for infile in filelist:
     if cnt >= testNum:
-        img_data = numpy.array(Image.open(dir_path+infile)).reshape(1, 65536)
-        data = numpy.row_stack((data, img_data))
+        img_train_data = numpy.array(Image.open(dir_path+infile)).reshape(1, 65536)
+        train_data = numpy.row_stack((train_data, img_train_data))
+        img_train_label = numpy.array(Image.open(raw_path+infile)).reshape(1, 65536)
+        train_label = numpy.row_stack((train_label, img_train_label))
     else:
-        img_label = numpy.array(Image.open(dir_path+infile)).reshape(1, 65536)
-        label = numpy.row_stack((data, img_label))
+        img_test_data = numpy.array(Image.open(dir_path + infile)).reshape(1, 65536)
+        test_data = numpy.row_stack((test_data, img_test_data))
+        img_test_label = numpy.array(Image.open(raw_path + infile)).reshape(1, 65536)
+        test_label = numpy.row_stack((test_label, img_test_label))
+
     cnt = cnt+1
     print(cnt)
 
 
-sio.savemat(mat_path+mat_name, {'data': data, 'label': label})
+sio.savemat(mat_path+'train.mat', {'data': train_data, 'label': train_label})
+sio.savemat(mat_path+'test.mat', {'data': test_data, 'label': test_label})
