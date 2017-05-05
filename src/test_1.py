8badce1fa1b31ec9b7e67ea14a02c99a35827c74
LIJING <lijinging@outlook.com>
2017-05-05 12:49:06 +0800

    扩充数据集并修改训练参数

--- src/test_1.py
+++ src/test_1.py
@@ -59,55 +59,28 @@ x_image = tf.reshape(x, [-1, 256, 256, 1])
 W_conv1 = weight_variable([5, 5, 1, 24])  # 第一层卷积层
 b_conv1 = bias_variable([24])  # 第一层卷积层的偏置量
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-#h_pool1 = max_pool_2x2(h_conv1)  # 第一次池化层
-
-#W_conv2 = weight_variable([5, 5, 20, 50])  # 第二次卷积层
-#b_conv2 = bias_variable([50])  # 第二层卷积层的偏置量
-#h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-#h_pool2 = max_pool_2x2(h_conv2)  # 第二曾池化层
-
-# W_conv2 = weight_variable([5, 5, 20, 1])  # 第二次卷积层
-# b_conv2 = bias_variable([1])  # 第二层卷积层的偏置量
-# h_conv2 = tf.nn.tanh(conv2d(h_conv1, W_conv2) + b_conv2)
-# #h_conv2 = conv2d(h_conv1, W_conv2) + b_conv2
-# y = tf.reshape(h_conv2, [-1, 65536])
 
 W_conv2 = weight_variable([5, 5, 24, 24])  # 第二次卷积层
 b_conv2 = bias_variable([24])  # 第二层卷积层的偏置量
 h_conv2 = tf.nn.relu(batchnormalize(conv2d(h_conv1, W_conv2) + b_conv2))
-#h_conv2 = conv2d(h_conv1, W_conv2) + b_conv2
 
 W_conv3 = weight_variable([5, 5, 24, 24])  # 第三次卷积层
 b_conv3 = bias_variable([24])  # 第二层卷积层的偏置量
 h_conv3 = tf.nn.relu(batchnormalize(conv2d(h_conv2, W_conv3) + b_conv3))
-#h_conv2 = conv2d(h_conv1, W_conv2) + b_conv2
 
 W_conv4 = weight_variable([5, 5, 24, 1])  # 第四次卷积层
 b_conv4 = bias_variable([1])  # 第二层卷积层的偏置量
-#h_conv4 = tf.nn.tanh(conv2d(h_conv3, W_conv4) + b_conv4)
 h_conv4 = conv2d(h_conv3, W_conv4) + b_conv4
 y = tf.reshape(h_conv4, [-1, 65536])
 
-#W_fc1 = weight_variable([2 * 2 * 50, 500])  # 全连接层
-#b_fc1 = bias_variable([500])  # 偏置量
-#h_pool2_flat = tf.reshape(h_pool2, [-1, 2 * 2 * 50])
-#h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-#
-
 keep_prob = tf.placeholder("float")
-
-#h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-#
-#W_fc2 = weight_variable([500, 32])
-#b_fc2 = bias_variable([32])
-#y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.8
 sess = tf.InteractiveSession(config=config)
 
 saver = tf.train.Saver()
-save_path = r"..\model\model_100.ckpt"
+save_path = r"..\model_1\model_420.ckpt"
 saver.restore(sess, save_path)
 
 '''
@@ -147,47 +120,10 @@ for i in range(pred.shape[0]):
         elif pred[i][j]>255:
             pred[i][j] = 255
 
-def testImg(infile = '../pic_gauss/lena.png', infile_raw = '../pic_raw/lena.png', filename = 'lena'):
-    im_test = np.array(Image.open(infile).convert('L')).reshape(1, 65536)
-    scipy.misc.imsave(infile, im_test.reshape(256, 256))
-    im_test = im_test.astype('float32') / 255.0
-
-    im_label = np.array(Image.open(infile_raw).convert('L')).reshape(1, 65536)
-
-    '''开始预测'''
-    pred = sess.run(y, feed_dict={x: im_test, keep_prob: 1.0})
-    im_out = im_test - pred
-    im_out = im_out * 255.0
-    im_out = im_out.astype(int)
-    for i in range(im_out.shape[0]):
-        for j in range(im_out.shape[1]):
-            if im_out[i][j] < 0:
-                im_out[i][j] = 0
-            elif im_out[i][j] > 255:
-                im_out[i][j] = 255
-    pred = pred * 255.0
-    pred = pred.astype(int)
-    for i in range(pred.shape[0]):
-        for j in range(pred.shape[1]):
-            if pred[i][j] < 0:
-                pred[i][j] = -pred[i][j]
-            elif pred[i][j] > 255:
-                pred[i][j] = 255
-    scipy.misc.imsave('../vis/0'+filename+'_test.jpg', im_test.reshape(256, 256))
-    scipy.misc.imsave('../vis/1'+filename+'.jpg', im_out.reshape(256, 256))
-    scipy.misc.imsave('../vis/3'+filename+'_noise.jpg', pred.reshape(256, 256))
-    scipy.misc.imsave('../vis/2'+filename+'_label.jpg', im_label.reshape(256, 256))
-
-    print("Before   ", end=':')
-    getSNR.getSNR(Image.open(r'../vis/0lena_test.jpg').convert('L'), Image.open(r'../vis/2lena_label.jpg').convert('L'))
-    print("After    ", end=':')
-    getSNR.getSNR(Image.open(r'../vis/1lena.jpg').convert('L'), Image.open(r'../vis/2lena_label.jpg').convert('L'))
-
 
 scipy.misc.imsave('../vis/1lena.jpg', im_out.reshape(256,256))
 scipy.misc.imsave('../vis/3lena_noise.jpg', pred.reshape(256,256))
 scipy.misc.imsave('../vis/2lena_label.jpg', im_label.reshape(256,256))
-
 
 print(pred)
 print(im_out)
@@ -198,12 +134,8 @@ print("After    ", end=':')
 getSNR.getSNR(Image.open(r'../vis/1lena.jpg').convert('L'), Image.open(r'../vis/2lena_label.jpg').convert('L'))
 print("Ps       ", end=':')
 getSNR.getSNR(Image.open(r'../vis/lena_ps.jpg').convert('L'), Image.open(r'../vis/2lena_label.jpg').convert('L'))
-#print(LABELS[np.argmax(pred, 1)])
-#print(LABELS[y_])
-print("----------------------------------")
 
 
-testImg(infile=r'../pic_gauss/279.png', infile_raw=r'../pic_raw/279.png', filename='new')
 
 
 sess.close()
--- src/train_1.py
+++ src/train_1.py
@@ -79,50 +79,31 @@ x_image = tf.reshape(x, [-1, 256, 256, 1])
 W_conv1 = weight_variable([5, 5, 1, 24])  # 第一层卷积层
 b_conv1 = bias_variable([24])  # 第一层卷积层的偏置量
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-#h_conv1 = conv2d(x_image, W_conv1) + b_conv1
-#h_pool1 = max_pool_2x2(h_conv1)  # 第一次池化层
-
-#W_conv2 = weight_variable([5, 5, 20, 50])  # 第二次卷积层
-#b_conv2 = bias_variable([50])  # 第二层卷积层的偏置量
-#h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-#h_pool2 = max_pool_2x2(h_conv2)  # 第二曾池化层
-
 
 W_conv2 = weight_variable([5, 5, 24, 24])  # 第二次卷积层
 b_conv2 = bias_variable([24])  # 第二层卷积层的偏置量
 h_conv2 = tf.nn.relu(batchnormalize(conv2d(h_conv1, W_conv2) + b_conv2))
-#h_conv2 = conv2d(h_conv1, W_conv2) + b_conv2
 
 W_conv3 = weight_variable([5, 5, 24, 24])  # 第三次卷积层
 b_conv3 = bias_variable([24])  # 第二层卷积层的偏置量
 h_conv3 = tf.nn.relu(batchnormalize(conv2d(h_conv2, W_conv3) + b_conv3))
-#h_conv2 = conv2d(h_conv1, W_conv2) + b_conv2
 
 W_conv4 = weight_variable([5, 5, 24, 1])  # 第四次卷积层
 b_conv4 = bias_variable([1])  # 第二层卷积层的偏置量
-#h_conv4 = tf.nn.tanh(conv2d(h_conv3, W_conv4) + b_conv4)
 h_conv4 = conv2d(h_conv3, W_conv4) + b_conv4
 y = tf.reshape(h_conv4, [-1, 65536])
 
-#W_fc1 = weight_variable([256 * 256 * 20, 65536])  # 全连接层
-#b_fc1 = bias_variable([65536])  # 偏置量
-#h_pool2_flat = tf.reshape(h_conv1, [-1, 256 * 256 * 20])
-#h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
-#h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-#
-#W_fc2 = weight_variable([65536, 65536])
-#b_fc2 = bias_variable([65536])
-#y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-cross_entropy = tf.reduce_sum((y_ - y)**2)
-train_step = tf.train.AdamOptimizer(2e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(cross_entropy, "float"))
+#cross_entropy = tf.reduce_sum((y_ - y)**2)
+#train_step = tf.train.AdamOptimizer(2e-4).minimize(cross_entropy)
+#loss = tf.reduce_mean(tf.cast(cross_entropy, "float"))
+loss = tf.reduce_mean(y_) - tf.reduce_mean(y)
+train_step = tf.train.AdamOptimizer(2e-3).minimize(loss)
 saver = tf.train.Saver()
-#sess.run(tf.global_variables_initializer())
-saver.restore(sess, r"..\model\model.ckpt")
+sess.run(tf.global_variables_initializer())
+#saver.restore(sess, r"..\model_1\model.ckpt")
 
 
 def next_batch(data, label, begin, length):
@@ -137,23 +118,23 @@ def next_batch(data, label, begin, length):
     return add
 
 
-for i in range(9000):
+for i in range(4500):
     size = 50
     # batch = mnist.train.next_batch(100)
     batch = next_batch(train_data, train_label, i * size, size)
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
     if i % 5 == 0:
         print(i, end=":")
-        print("test loss %g" % accuracy.eval(feed_dict={
+        print("test loss %g" % loss.eval(feed_dict={
             x: test_data, y_: test_label, keep_prob: 1.0}))
     if i % 20 == 0:
 
-        save_path = r"..\model\model_%d.ckpt" % i
+        save_path = r"..\model_1\model_%d.ckpt" % i
         saver.save(sess, save_path)
 
 
 
-save_path = r"..\model\model.ckpt"
+save_path = r"..\model_1\model.ckpt"
 saver.save(sess, save_path)
 
 
